c778ff1e64dbddb3418a8bfde121eface788fd5b
BL <bouchkhaf@gmail.com>
2024-01-18 16:41:02 +0100

    Mise au point du service de configuration

---
+++ src/test/java/fr/devavance/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devavance.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
